f3c399538fefe72bacd50e48fe81c9afe719b6e6
Star <65995358+StarMKWii@users.noreply.github.com>
2021-05-14 14:20:01 -0400

    Added a CRC32 calculation function.

---
+++ title_loader/include/utils.h
@@ -0,0 +1,10 @@
+/*!
+ * Author  : Star
+ * Date    : 14 May 2021
+ * File    : utils.h
+ * Version : 1.0.0.0
+ */
+
+#pragma once
+
+#include <utils/crc32.h>
---
+++ title_loader/include/utils/crc32.h
@@ -0,0 +1,39 @@
+/*!
+ * Author  : Star
+ * Date    : 14 May 2021
+ * File    : crc32.h
+ * Version : 1.0.0.0
+ */
+
+#pragma once
+
+//! Definitions
+#define POLYNOMIAL 0xEDB88320
+#define XOR_CRYPT_POLYNOMIAL(polynomial) ((polynomial) ^ 0x53746172)
+
+//! Function Definitions
+
+//! Public Functions
+/*---------------------------------------------------------------------------*
+ * Name        : CRC32_Calculate
+ * Description : Calculates the CRC32 checksum of a buffer.
+ * Arguments   : uiCRC32           The initial value of the CRC32 checksum.
+ *               buffer            A pointer to a buffer.
+ *               uiBufferLength    The length of the buffer.
+ * Returns     : The CRC32 checksum of the buffer.
+ *---------------------------------------------------------------------------*/
+__attribute__((always_inline))
+inline unsigned int
+CRC32_Calculate(unsigned int uiCRC32, const void* buffer, unsigned int uiBufferLength)
+{
+	const volatile unsigned int vuiPolynomial = XOR_CRYPT_POLYNOMIAL(POLYNOMIAL);
+
+	uiCRC32 = ~uiCRC32;
+	for (unsigned char* pBuffer = (unsigned char*)buffer; uiBufferLength != 0; uiBufferLength--)
+	{
+		uiCRC32 ^= *pBuffer++;
+		for (int i = 0; i < 8; i++)
+			uiCRC32 = (uiCRC32 >> 1) ^ (XOR_CRYPT_POLYNOMIAL(vuiPolynomial) & -(uiCRC32 & 1));
+	}
+	return ~uiCRC32;
+}
