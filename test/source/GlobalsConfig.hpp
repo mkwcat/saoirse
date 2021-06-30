90af54ca44a35c5367f43ab28bcb8ee5aa4328ab
riidefi <34194588+riidefi@users.noreply.github.com>
2021-06-29 19:37:18 -0600

    Add heap alloc functions

--- test/source/GlobalsConfig.hpp
+++ test/source/GlobalsConfig.hpp
@@ -8,3 +8,22 @@ uint32_t GetArenaLow();
 void SetArenaLow(uint32_t low);
 uint32_t GetArenaHigh();
 void SetArenaHigh(uint32_t high);
+
+namespace mem {
+	inline void* AllocFromArenaHigh(uint32_t size) {
+		const uint32_t current_High = GetArenaHigh();
+		SetArenaHigh(current_High + size);
+
+		return reinterpret_cast<void*>(current_High);
+	}
+
+	struct arena_high {};
+}
+
+
+inline void* operator new(uint32_t size, mem::arena_high) {
+	return mem::AllocFromArenaHigh(size);
+}
+inline void* operator new[](uint32_t size, mem::arena_high) {
+	return mem::AllocFromArenaHigh(size);
+}
