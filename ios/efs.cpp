b7d3310924a31df1921134013092c4bd659c994b
riidefi <34194588+riidefi@users.noreply.github.com>
2021-08-26 17:42:57 -0600

    Refactor Star code

--- ios/efs.cpp
+++ ios/efs.cpp
@@ -1,35 +1,37 @@
-#include <types.h>
-#include <main.h>
+#include <disk.h>
+#include <ff.h>
 #include <ios.h>
+#include <main.h>
 #include <nand.h>
 #include <os.h>
-#include <ff.h>
-#include <disk.h>
 #include <sdcard.h>
+#include <types.h>
 
-#include <cstring>
+#include <algorithm>
+#include <array>
 #include <cstdio>
+#include <cstring>
 
-namespace EFS
-{
+namespace EFS {
 
 #define EFS_DRIVE "0:/"
 
-static FIL* spFileDescriptorArray[NAND_MAX_FILE_DESCRIPTOR_AMOUNT];
+static std::array<FIL*, NAND_MAX_FILE_DESCRIPTOR_AMOUNT> spFileDescriptorArray;
 
 /*---------------------------------------------------------------------------*
  * Name        : IsFileDescriptorValid
  * Description : Checks if a file descriptor is valid.
  * Arguments   : fd    The file descriptor to check.
- * Returns     : 0     The file descriptor is invalid.
- *               1     The file descriptor is valid.
+ * Returns     : If the file descriptor is valid.
  *---------------------------------------------------------------------------*/
-static bool IsFileDescriptorValid(int fd)
-{
-    if (fd < 0 || fd > NAND_MAX_FILE_DESCRIPTOR_AMOUNT)
+static bool IsFileDescriptorValid(int fd) {
+    if (fd < 0 || fd > static_cast<int>(spFileDescriptorArray.size()))
         return false;
 
-    return (spFileDescriptorArray[fd] != NULL);
+    if (spFileDescriptorArray[fd] == nullptr)
+        return false;
+
+    return true;
 }
 
 /*---------------------------------------------------------------------------*
@@ -38,41 +40,39 @@ static bool IsFileDescriptorValid(int fd)
  * Returns     : An available index in the file descriptor array, or -1 if
  *               there is no index available in the file descriptor array.
  *---------------------------------------------------------------------------*/
-static int GetAvailableFileDescriptor()
-{
-    for (int i = 0; i < NAND_MAX_FILE_DESCRIPTOR_AMOUNT; i++)
-        if (spFileDescriptorArray[i] == NULL)
-            return i;
-    return -1;
+static int GetAvailableFileDescriptor() {
+    auto it = std::find(spFileDescriptorArray.begin(),
+                        spFileDescriptorArray.end(), nullptr);
+
+    return it == spFileDescriptorArray.end()
+               ? -1
+               : it - spFileDescriptorArray.begin();
 }
 
 /*---------------------------------------------------------------------------*
- * Name        : IsFilenameValid
+ * Name        : IsFilenameDOS83
  * Description : Checks if a filename is a valid 8.3 filename.
  * Arguments   : filename    The filename to check.
- * Returns     : 0           The filename is not a valid 8.3 filename.
- *               1           The filename is a valid 8.3 filename.
+ * Returns     : If the filename is a valid 8.3 filename.
  *---------------------------------------------------------------------------*/
-static bool IsFilenameValid(const char* filename)
-{
-    const int MIN_83_FILENAME_LENGTH           = 1;
-    const int MAX_83_FILENAME_LENGTH           = 8;
+static bool IsFilenameDOS83(const char* filename) {
+    const int MIN_83_FILENAME_LENGTH = 1;
+    const int MAX_83_FILENAME_LENGTH = 8;
     const int MAX_83_FILENAME_EXTENSION_LENGTH = 3;
 
-    enum
-    {
-        CHARACTER_SPACE  = 0x20,
+    enum {
+        CHARACTER_SPACE = 0x20,
         CHARACTER_PERIOD = 0x2E,
         CHARACTER_DELETE = 0x7F
     };
 
-    int filenameLength;
-    const char* filenameExtension = NULL;
-    for (filenameLength = 0; filename[filenameLength]; filenameLength++)
-    {
-        char c = filename[filenameLength];
+    int filenameLength = strlen(filename);
+    const char* filenameExtension = nullptr;
+    for (int i = 0; i < filenameLength; i++) {
+        const char c = filename[i];
 
-        // A 8.3 filename must only contain characters that can be represented in ASCII, in the range below 0x80
+        // A 8.3 filename must only contain characters that can be represented
+        // in ASCII, in the range below 0x80
         if ((unsigned char)c > CHARACTER_DELETE)
             return false;
 
@@ -81,18 +81,17 @@ static bool IsFilenameValid(const char* filename)
             return false;
 
         // A 8.3 filename must not contain more than one period character
-        if (c == CHARACTER_PERIOD)
-        {
+        if (c == CHARACTER_PERIOD) {
             if (filenameExtension)
                 return false;
 
-            filenameExtension = &filename[filenameLength + 1];
+            filenameExtension = &filename[i + 1];
         }
     }
-    
-    size_t filenameExtensionLength;
-    if (filenameExtension && (filenameExtensionLength = strlen(filenameExtension)))
-    {
+
+    if (filenameExtension) {
+        const size_t filenameExtensionLength = strlen(filenameExtension);
+
         // Don't include the length of the extension in the filename length
         filenameLength = ((filenameExtension - 1) - filename);
 
@@ -100,15 +99,14 @@ static bool IsFilenameValid(const char* filename)
         if (filenameExtensionLength > MAX_83_FILENAME_EXTENSION_LENGTH)
             return false;
     }
-    
+
     // The base filename must be 1-8 characters in length
-    return (filenameLength >= MIN_83_FILENAME_LENGTH && filenameLength <= MAX_83_FILENAME_LENGTH);
+    return (filenameLength >= MIN_83_FILENAME_LENGTH &&
+            filenameLength <= MAX_83_FILENAME_LENGTH);
 }
 
-static s32 ForwardRequest(s32 fd, IOS::Request* req)
-{
-    switch (req->cmd)
-    {
+static s32 ForwardRequest(s32 fd, IOS::Request* req) {
+    switch (req->cmd) {
     case IOS::Command::Close:
         return IOS_Close(fd);
     case IOS::Command::Read:
@@ -126,23 +124,22 @@ static s32 ForwardRequest(s32 fd, IOS::Request* req)
     }
 }
 
-static s32 FResultToISFSError(FRESULT fret)
-{
+static s32 FResultToISFSError(FRESULT fret) {
     switch (fret) {
     case FR_OK:
         return ISFSError::OK;
-    
+
     case FR_INVALID_NAME:
     case FR_INVALID_DRIVE:
     case FR_INVALID_PARAMETER:
     case FR_INVALID_OBJECT:
         return ISFSError::Invalid;
-    
+
     case FR_DISK_ERR:
     case FR_INT_ERR:
     case FR_NO_FILESYSTEM:
         return ISFSError::Corrupt;
-    
+
     case FR_NOT_READY:
     case FR_NOT_ENABLED:
         return ISFSError::NotReady;
@@ -150,11 +147,11 @@ static s32 FResultToISFSError(FRESULT fret)
     case FR_NO_FILE:
     case FR_NO_PATH:
         return ISFSError::NotFound;
-        
+
     case FR_DENIED:
     case FR_WRITE_PROTECTED:
         return ISFSError::NoAccess;
-    
+
     case FR_EXIST:
         return ISFSError::Exists;
 
@@ -163,7 +160,7 @@ static s32 FResultToISFSError(FRESULT fret)
 
     case FR_TOO_MANY_OPEN_FILES:
         return ISFSError::MaxOpen;
-    
+
     case FR_MKFS_ABORTED:
     case FR_NOT_ENOUGH_CORE:
     case FR_TIMEOUT:
@@ -172,12 +169,11 @@ static s32 FResultToISFSError(FRESULT fret)
     }
 }
 
-/* 
+/*
  * Handle open request from the filesystem proxy.
  * Returns: File descriptor, or ISFS error code.
  */
-static s32 ReqOpen(const char* path, u32 mode)
-{
+static s32 ReqOpen(const char* path, u32 mode) {
     int fd = GetAvailableFileDescriptor();
     if (fd < 0)
         return ISFSError::MaxOpen;
@@ -187,39 +183,48 @@ static s32 ReqOpen(const char* path, u32 mode)
 
     if (path[0] != NAND_DIRECTORY_SEPARATOR_CHAR)
         return ISFSError::Invalid;
-    
+
     if (strlen(path) >= NAND_MAX_FILEPATH_LENGTH)
         return ISFSError::Invalid;
-    
+
     if (mode > IOS_OPEN_RW)
         return ISFSError::Invalid;
 
-    // The second check guarantees that there will be at least one instance of the "NAND_DIRECTORY_SEPARATOR_CHAR" character in the character string
+    // The second check guarantees that there will be at least one instance of
+    // the "NAND_DIRECTORY_SEPARATOR_CHAR" character in the character string
     const char* filename = strrchr(path, NAND_DIRECTORY_SEPARATOR_CHAR) + 1;
-    if (!IsFilenameValid(filename))
-    {
-        peli::Log(LogL::ERROR, "[EFS::ReqOpen] Filename '%s' is invalid !", filename);
+    if (!IsFilenameDOS83(filename)) {
+        peli::Log(LogL::ERROR, "[EFS::ReqOpen] Filename '%s' is invalid !",
+                  filename);
         return ISFSError::Invalid;
     }
 
     // Create the filepath to open
     char efsFilepath[NAND_MAX_FILENAME_LENGTH + sizeof(EFS_DRIVE)];
-    snprintf(efsFilepath, NAND_MAX_FILENAME_LENGTH + sizeof(EFS_DRIVE), EFS_DRIVE "%s", filename);
+    const int ret =
+        snprintf(efsFilepath, sizeof(efsFilepath), EFS_DRIVE "%s", filename);
+    if (ret <= 0) {
+        // It's possible if we remove the DOS83 check, or if it breaks, we could
+        // smash the stack here.
+        peli::Log(LogL::ERROR, "[EFS::ReqOpen] Failed to format");
+        return ISFSError::Invalid;
+    }
 
     spFileDescriptorArray[fd] = new FIL;
     ASSERT(IsFileDescriptorValid(fd));
     const FRESULT fret = f_open(spFileDescriptorArray[fd], efsFilepath, mode);
-    if (fret != FR_OK)
-    {
+    if (fret != FR_OK) {
         peli::Log(LogL::ERROR,
-            "[EFS::ReqOpen] Failed to open file '%s', error: %d",
-            efsFilepath, fret);
-        
+                  "[EFS::ReqOpen] Failed to open file '%s', error: %d",
+                  efsFilepath, fret);
+
         delete spFileDescriptorArray[fd];
         return FResultToISFSError(fret);
     }
-    
-    peli::Log(LogL::INFO, "[EFS::ReqOpen] Successfully opened file '%s' (fd=%d, mode=%d) !", efsFilepath, fd, mode);
+
+    peli::Log(LogL::INFO,
+              "[EFS::ReqOpen] Successfully opened file '%s' (fd=%d, mode=%d) !",
+              efsFilepath, fd, mode);
 
     return fd;
 }
@@ -228,21 +233,21 @@ static s32 ReqOpen(const char* path, u32 mode)
  * Close open file descriptor.
  * Returns: 0 for success, or IOS error code.
  */
-static s32 ReqClose(s32 fd)
-{
+static s32 ReqClose(s32 fd) {
     if (!IsFileDescriptorValid(fd))
         return ISFSError::Invalid;
 
-    if (f_close(spFileDescriptorArray[fd]) != FR_OK)
-    {
-        peli::Log(LogL::ERROR, "[EFS::ReqClose] Failed to close file descriptor %d !", fd);
+    if (f_close(spFileDescriptorArray[fd]) != FR_OK) {
+        peli::Log(LogL::ERROR,
+                  "[EFS::ReqClose] Failed to close file descriptor %d !", fd);
         return ISFSError::Unknown;
     }
 
     delete spFileDescriptorArray[fd];
     spFileDescriptorArray[fd] = nullptr;
 
-    peli::Log(LogL::INFO, "[EFS::ReqClose] Successfully closed file descriptor %d !", fd);
+    peli::Log(LogL::INFO,
+              "[EFS::ReqClose] Successfully closed file descriptor %d !", fd);
 
     return ISFSError::OK;
 }
@@ -251,8 +256,7 @@ static s32 ReqClose(s32 fd)
  * Read data from open file descriptor.
  * Returns: Amount read, or ISFS error code.
  */
-static s32 ReqRead(s32 fd, void* data, u32 len)
-{
+static s32 ReqRead(s32 fd, void* data, u32 len) {
     if (!IsFileDescriptorValid(fd))
         return ISFSError::Invalid;
 
@@ -260,10 +264,9 @@ static s32 ReqRead(s32 fd, void* data, u32 len)
         return ISFSError::OK;
 
     unsigned int bytesRead;
-    const FRESULT fret
-        = f_read(spFileDescriptorArray[fd], data, len, &bytesRead);
-    if (fret != FR_OK)
-    {
+    const FRESULT fret =
+        f_read(spFileDescriptorArray[fd], data, len, &bytesRead);
+    if (fret != FR_OK) {
         peli::Log(LogL::ERROR,
                   "[EFS::ReqRead] Failed to read %d bytes from file descriptor "
                   "%d, error: %d",
@@ -271,7 +274,10 @@ static s32 ReqRead(s32 fd, void* data, u32 len)
         return FResultToISFSError(fret);
     }
 
-    peli::Log(LogL::INFO, "[EFS::ReqRead] Successfully read %d bytes from file descriptor %d !", bytesRead, fd);
+    peli::Log(
+        LogL::INFO,
+        "[EFS::ReqRead] Successfully read %d bytes from file descriptor %d !",
+        bytesRead, fd);
 
     return bytesRead;
 }
@@ -280,19 +286,17 @@ static s32 ReqRead(s32 fd, void* data, u32 len)
  * Write data to open file descriptor.
  * Returns: Amount wrote, or ISFS error code.
  */
-static s32 ReqWrite(s32 fd, const void* data, u32 len)
-{
+static s32 ReqWrite(s32 fd, const void* data, u32 len) {
     if (!IsFileDescriptorValid(fd))
         return ISFSError::Invalid;
-    
+
     if (len == 0)
         return ISFSError::OK;
 
     unsigned int bytesWrote;
-    const FRESULT fret
-        = f_write(spFileDescriptorArray[fd], data, len, &bytesWrote);
-    if (fret != FR_OK)
-    {
+    const FRESULT fret =
+        f_write(spFileDescriptorArray[fd], data, len, &bytesWrote);
+    if (fret != FR_OK) {
         peli::Log(LogL::ERROR,
                   "[EFS::ReqWrite] Failed to write %d bytes to file descriptor "
                   "%d, error: %d",
@@ -300,40 +304,41 @@ static s32 ReqWrite(s32 fd, const void* data, u32 len)
         return FResultToISFSError(fret);
     }
 
-    peli::Log(LogL::INFO, "[EFS::ReqWrite] Successfully wrote %d bytes to file descriptor %d !", bytesWrote, fd);
+    peli::Log(
+        LogL::INFO,
+        "[EFS::ReqWrite] Successfully wrote %d bytes to file descriptor %d !",
+        bytesWrote, fd);
 
     return bytesWrote;
 }
 
-static s32 IPCRequest(IOS::Request* req)
-{
+static s32 IPCRequest(IOS::Request* req) {
     s32 ret = IOSErr::Invalid;
 
     if (req->cmd != IOS::Command::Open && req->fd >= 100) {
         return ForwardRequest(req->fd - 100, req);
     }
 
-    switch (req->cmd)
-    {
+    switch (req->cmd) {
     case IOS::Command::Open:
         ret = ReqOpen(req->open.path, req->open.mode);
         break;
-    
+
     case IOS::Command::Close:
         ret = ReqClose(req->fd);
         break;
-    
+
     case IOS::Command::Read:
         ret = ReqRead(req->fd, req->read.data, req->read.len);
         break;
-    
+
     case IOS::Command::Write:
         ret = ReqWrite(req->fd, req->write.data, req->write.len);
         break;
-    
+
     default:
-        peli::Log(LogL::ERROR,
-            "EFS: Unknown command: %u", static_cast<u32>(req->cmd));
+        peli::Log(LogL::ERROR, "EFS: Unknown command: %u",
+                  static_cast<u32>(req->cmd));
         break;
     }
 
@@ -347,16 +352,14 @@ static s32 IPCRequest(IOS::Request* req)
     return ret;
 }
 
-static void OpenTestFile()
-{
+static void OpenTestFile() {
     /* We must attempt to open a file first for FatFS to function properly */
     FIL testFile;
     FRESULT fret = f_open(&testFile, "0:/", FA_READ);
     peli::Log(LogL::INFO, "Test open result: %d", fret);
 }
 
-extern "C" s32 FS_StartRM([[maybe_unused]] void* arg)
-{
+extern "C" s32 FS_StartRM([[maybe_unused]] void* arg) {
     peli::Log(LogL::INFO, "Starting FS...");
 
     if (!SDCard::Open()) {
@@ -373,7 +376,7 @@ extern "C" s32 FS_StartRM([[maybe_unused]] void* arg)
     const s32 ret = IOS_RegisterResourceManager("", queue.id());
     if (ret != IOSErr::OK) {
         peli::Log(LogL::ERROR,
-            "FS_StartRM: IOS_RegisterResourceManager failed: %d", ret);
+                  "FS_StartRM: IOS_RegisterResourceManager failed: %d", ret);
         abort();
     }
 
@@ -385,4 +388,4 @@ extern "C" s32 FS_StartRM([[maybe_unused]] void* arg)
     return 0;
 }
 
-}
+} // namespace EFS
